b54fdae9ef11812602ae4273a436351fd9088bf8
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-10-10 21:23:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,7 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
+API_ID       = 21518327
+API_HASH     = "e72f588b3e4763f01eecfc3c4aa7e8ac"
+BOT_TOKEN    = "6017531522:AAGL1Pj2ksRr56clZLYyCt8mvd5MH60tis"SESSION      = "BQFIV_cAqc94-dSpt3quw7r5b2iLH_NUvhO9v6msqO8LHPAHQpWFpGF7mDDQIcad96UlKR_Nj5Jhi4SqMZieyPdobKwwU9oINGfFNyf4wqe-CGUpR0NUmsINZUfD2QOGiWhSO3yP7Trd8r3zwHxykTDBM3c5whnWNNeNwC9TtHVL_yEUMMpUDSM5o_XdhB-5aZ52vnfz6797iOsimRJKlfIxpREi8PkiX7O7erXI9922kUHr6lOfCvanAxH4DfyAZS0GS3RwDvLyIHAQYVo3Q3FC6Eyk-Ptqx4a8gkn-INwkcDOfdqjDi-1irZ8jjFVlW86fx1d-pGX1ny8xroT_b_vCzur16AAAAAFWPrmkAA"
+DATABASE_URI = "mongodb+srv://vinayjaat698:<vinayjaat698>@cluster1.hbafc0k.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001839965169
+ADMIN        = 5741918628
 
