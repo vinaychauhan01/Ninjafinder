0ac5f27a3f89f3ca754e6d3e0bd8b5480f5f055e
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-10-14 18:35:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,7 @@
 API_ID       = 21518327
 API_HASH     = "e72f588b3e4763f01eecfc3c4aa7e8ac"
 BOT_TOKEN    = "6017531522:AAGL1Pj2ksRr56clZLYyCt8mvd5MH60tis8"
-SESSION      = "
-BQFIV_cAJg1Pg6M-9YmkCfZYNPb-8ZRow5it4ugo3SM-gDt-oNTeojKRLG3j8hCrxPbdkNAJdsbE56Iupya4zAssyfNAh99ReAwTrFFZgSyGMGTKIS5MtGtU1ev8zo_ufhvloBptlJK-TCJu7jcEMfFRdU1Un2UpKyOp3MozS7_mPcquVGpqyU9b_kfk8dfralZbehbUvj9rYfgM_uohv8cEm4XLcYA-KNI9_YQsIak_DW7Mj4RthdjOPdcc5xSYzojnU-DuIT6JJK1mk3-2f0VQQ8n-tDGWu6mqAc-U2psHx-NtXAjIkSWU4wUO7XZ8yQK_e3MqFA3_mm2Yv4kK9LDSaiV-qAAAAAFWPrmkAA "
+SESSION      = "BQFIV_cAJg1Pg6M-9YmkCfZYNPb-8ZRow5it4ugo3SM-gDt-oNTeojKRLG3j8hCrxPbdkNAJdsbE56Iupya4zAssyfNAh99ReAwTrFFZgSyGMGTKIS5MtGtU1ev8zo_ufhvloBptlJK-TCJu7jcEMfFRdU1Un2UpKyOp3MozS7_mPcquVGpqyU9b_kfk8dfralZbehbUvj9rYfgM_uohv8cEm4XLcYA-KNI9_YQsIak_DW7Mj4RthdjOPdcc5xSYzojnU-DuIT6JJK1mk3-2f0VQQ8n-tDGWu6mqAc-U2psHx-NtXAjIkSWU4wUO7XZ8yQK_e3MqFA3_mm2Yv4kK9LDSaiV-qAAAAAFWPrmkAA"
 DATABASE_URI = "mongodb+srv://vinayjaat:vinay123@vinay1.3yzdb9g.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001839965169
 ADMIN        = 57419186289
